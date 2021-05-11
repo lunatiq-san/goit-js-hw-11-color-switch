153322f0deb4f15da551d1218f11e421cb0764a5
Rozumiak Vladyslav <lunatiq86@gmail.com>
2021-05-11 17:30:27 +0300

    Finish color switch

--- index.js
+++ index.js
@@ -7,7 +7,30 @@ const colors = [
   '#795548',
 ];
 
-// Generator random number
+const refs = {
+  startBtn: document.querySelector('[data-action="start"]'),
+  stopBtn: document.querySelector('[data-action="stop"]'),
+  body: document.body,
+};
+
+// random number generator
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
+let timerId = null;
+
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {
+  timerId = setInterval(() => {
+    refs.body.style.backgroundColor =
+      colors[randomIntegerFromInterval(0, colors.length)];
+  }, 1000);
+  refs.startBtn.disabled = true;
+}
+
+function onStopBtnClick() {
+  clearInterval(timerId);
+  refs.startBtn.disabled = false;
+}
